445ad524b4d3ead5576b415c6debe3d706249e82
nikitaz <nikitaz@earnix.com>
2022-10-07 22:28:19 +0300

    People count example app

--- app/src/main/java/com/bonamash/exampleapplication/MainActivity.kt
+++ app/src/main/java/com/bonamash/exampleapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.bonamash.exampleapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+        val tvName = findViewById<TextView>(R.id.tvName)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener {
+            timesClicked++
+            tvName.text = timesClicked.toString()
+            Toast.makeText(this, "Toaster!!", Toast.LENGTH_LONG).show()
+        }
     }
 }
